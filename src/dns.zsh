c07d8a28eb232a2822dce8c890ca03f2db26ec60
Stephan Schuler <stephan.schuler@netlogix.de>
2020-11-03 09:33:17 +0100

    Flush dns cache removes configured DNS servers for Wi-Fi

--- src/dns.zsh
+++ src/dns.zsh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
 
-alias dns-flush="sudo killall -HUP mDNSResponder"
+alias dns-remove-servers="networksetup -setdnsservers Wi-Fi empty"
+alias dns-kill-dns="sudo killall -HUP mDNSResponder"
+alias dns-flush="dns-remove-servers && dns-kill-dns"
