1f589e11a914ed603fd9c40259650a91f717e807
Lars Lauger <lars.lauger@netlogix.de>
2023-06-07 09:41:13 +0200

    BUGFIX: Adjust php aliases to use "brew --prefix" instead of fixed path

--- src/php.zsh
+++ src/php.zsh
@@ -63,7 +63,7 @@ function _check_directory_for_php_version() {
 
     local versionFile=".phprc"
     local vagrantFile="Vagrantfile"
-    local comnposerFile="composer.json"
+    local composerFile="composer.json"
 
     if [[ -f "$searchPath/$versionFile" ]];
     then
@@ -77,10 +77,10 @@ function _check_directory_for_php_version() {
         _switch_best_matching_php_version "$phpVersion" "$searchPath/$vagrantFile" && return
     fi
 
-    if [[ -f "$searchPath/$comnposerFile" ]];
+    if [[ -f "$searchPath/$composerFile" ]];
     then
-        local phpVersion=`jshon -Q -e require -e php -u < $searchPath/$comnposerFile | sed -E "s/^[~>^]?([0-9])\.([0-9]+)$/\\1.\\2/g"`
-        _switch_best_matching_php_version "$phpVersion" "$searchPath/$comnposerFile" && return
+        local phpVersion=`jshon -Q -e require -e php -u < $searchPath/$composerFile | sed -E "s/^[~>^]?([0-9])\.([0-9]+)$/\\1.\\2/g"`
+        _switch_best_matching_php_version "$phpVersion" "$searchPath/$composerFile" && return
     fi
 
     _check_directory_for_php_version `dirname $searchPath`
@@ -91,15 +91,15 @@ function _check_current_directory_for_php_version() {
     _check_directory_for_php_version `pwd`
 }
 
-
 function() {
+    local brewPrefix=`eval-cached "brew --prefix"`
     local versionNumbers=`eval-cached "brew list --version | grep -E ^php | awk '{ print \\$2 }' | awk -F. '{ print \\$1\".\"\\$2}' | sort"`
     local versionNumber
 
     echo $versionNumbers | while IFS= read -r versionNumber ;
     do
         function() {
-            local executable=`eval-cached "brew info php@$versionNumber | grep -E -o '/usr/local/Cellar/[^[:space:]]+' | awk '{ print \\$0 \"/bin/php\" }'"`
+            local executable=`eval-cached "brew info php@$versionNumber | grep -E -o '$brewPrefix/Cellar/[^[:space:]]+' | awk '{ print \\$0 \"/bin/php\" }'"`
             local aliasname=`echo php$versionNumber | awk -F. '{ print $1$2}'`
             alias $aliasname=$executable
         }
